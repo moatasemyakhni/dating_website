35750863d1534ae777a12ad9b929a4b0aaa0428f
= <motasem.yakhni@gmail.com>
2022-10-03 15:28:39 +0300

    Frontend => store signup id values in vars

--- frontend/scripts/forms.js
+++ frontend/scripts/forms.js
@@ -1,3 +1,4 @@
+// call the forms
 const signupFormCaller = document.getElementById('signup-form-caller');
 const loginFormCallers = document.querySelectorAll('.login-form-caller');
 const signupSection = document.getElementById('signup-section');
@@ -20,3 +21,21 @@ loginFormCallers.forEach(loginFormCaller => {
         loginSection.classList.toggle('login-section');
     });
 });
+
+// take forms info
+const formsBaseUrl = baseUrl + "/auth";
+
+// signup info
+const fullName = document.getElementById('male-gender');
+const femaleGender = document.getElementById('female-gender');
+const age = document.getElementById('age');
+const signupEmail = document.getElementById('signup-email');
+const signupPassword = document.getElementById('signup-password');
+const passwordRepeat = document.getElementById('password-repeat');
+const country = document.getElementById('country-selector');
+const city = document.getElementById('city-selector');
+const interestMale = document.getElementById('male-gender-interest');
+const interestFemale = document.getElementById('female-gender-interest');
+const bio = document.getElementById('bio');
+const profilePhoto = document.getElementById('profile-photo');
+const signupBtn = document.getElementById('signup-submit-btn');
