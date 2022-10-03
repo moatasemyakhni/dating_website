95455a6a95e9cda6ccea9023f931676bf765fb66
= <motasem.yakhni@gmail.com>
2022-10-03 15:56:05 +0300

    Backend => update migartion that makes bio accepts null

--- backend/database/migrations/2022_10_03_123930_update_some_records_to_accept_null_in_users_table.php
+++ backend/database/migrations/2022_10_03_123930_update_some_records_to_accept_null_in_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
-            $table->string('bio', 255)->nullable();
+            $table->string('bio', 255)->nullable()->after('location');
         });
     }
 
