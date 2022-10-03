564f30ef172cec0ef66f93fcbc2a01b9a531cad6
= <motasem.yakhni@gmail.com>
2022-10-03 15:09:05 +0300

    Frontend => make forms interactive

--- frontend/scripts/forms.js
+++ frontend/scripts/forms.js
@@ -1,9 +1,22 @@
 const signupFormCaller = document.getElementById('signup-form-caller');
+const loginFormCallers = document.querySelectorAll('.login-form-caller');
 const signupSection = document.getElementById('signup-section');
 const loginSection = document.getElementById('login-section');
+
+
 signupFormCaller.addEventListener('click', () => {
     signupSection.classList.toggle('view-hidden');
     loginSection.classList.add('view-hidden');
+    loginSection.classList.remove('login-section');
     signupSection.classList.toggle('signup-section');
 
+});
+
+loginFormCallers.forEach(loginFormCaller => {
+    loginFormCaller.addEventListener('click', () => {
+        loginSection.classList.toggle('view-hidden');
+        signupSection.classList.add('view-hidden');
+        signupSection.classList.remove('signup-section');
+        loginSection.classList.toggle('login-section');
+    });
 });
