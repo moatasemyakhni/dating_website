be6a170ddc198528124bbcdea93dd877a51bc159
= <motasem.yakhni@gmail.com>
2022-10-05 22:05:58 +0300

    Frontend => create message function

--- frontend/scripts/chat.js
+++ frontend/scripts/chat.js
@@ -1,8 +1,41 @@
 const favSection = document.getElementById('favorite-main');
 const privateSection = document.getElementById('private-chat-section');
 const dmPageCaller = document.getElementById('dm-caller');
-
+const messages = document.querySelector('.messages');
 dmPageCaller.addEventListener('click', () => {
     privateSection.classList.add('view-none');
     favSection.classList.remove('view-none');
 });
+
+const createChatMessage = (id, img, fullName, timeDiff, content) => {
+    const messageDiv = document.createElement('div');
+    messageDiv.setAttribute('class', 'message');
+        const headerMessageDiv = document.createElement('div');
+        headerMessageDiv.setAttribute('class', 'header-message');
+            const imgMessage = document.createElement('div');
+            imgMessage.setAttribute('class', 'img-message');
+            imgMessage.setAttribute('id', `img-header-${id}`);
+            document.getElementById('id').style.backgroundImage = img;
+
+            const name = document.createElement('p');
+            name.textContent = fullName;
+
+            const sinceWhenMessageSent = document.createElement('p');
+            sinceWhenMessageSent.setAttribute('class', 'small');
+            sinceWhenMessageSent.textContent = timeDiff;
+        headerMessageDiv.appendChild(imgMessage);
+        headerMessageDiv.appendChild(name);
+        headerMessageDiv.appendChild(sinceWhenMessageSent);
+
+        const bodyMessageDiv = document.createElement('div');
+        bodyMessageDiv.setAttribute('class', 'body-message');
+            const bodyContent = document.createElement('p');
+            bodyContent.textContent = content;
+        bodyMessageDiv.appendChild(bodyContent);
+    
+    messageDiv.appendChild(headerMessageDiv);
+    messageDiv.appendChild(bodyMessageDiv);
+
+    messages.appendChild(messageDiv);
+
+}
