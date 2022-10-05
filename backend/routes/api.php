d0d497428925eb50f29fcbe0c086fa02dde41483
= <motasem.yakhni@gmail.com>
2022-10-05 09:17:16 +0300

    Backend => turn logout api from post to get

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,7 +10,7 @@ Route::group(['prefix' => 'auth'], function () {
 
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
-    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::get('me', [AuthController::class, 'me']);
 
